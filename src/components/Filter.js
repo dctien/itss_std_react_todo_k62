a251ffa989cd9fa7f2f880a493fd51d12d57a8c5
dctien <dctien00@gmail.com>
2021-04-13 15:44:07 +0700

    Add Tabs

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,9 +4,29 @@
 　・タブで表示する
 　・サポートするステータスは「すべて」「未完了」「完了済み」
 */
-function Filter(  ) {
+function Filter( {choose, onClickFilter} ) {
+  function clickFilter(params, e) {
+    e.preventDefault(); //prevent reloading browser
+    onClickFilter(params)
+  }
+
   return (
     <div className="panel-tabs">
+      <a 
+        href="/#" 
+        className={choose === 'すべて' ? 'is-active' : ''} 
+        onClick={clickFilter.bind(null, 'すべて')}
+      >すべて</a>
+      <a 
+        href="/#" 
+        className={choose === '未完了' ? 'is-active' : ''} 
+        onClick={clickFilter.bind(null, '未完了')}
+      >未完了</a>
+      <a 
+        href="/#" 
+        className={choose === '完了済み' ? 'is-active' : ''} 
+        onClick={clickFilter.bind(null, '完了済み')}
+      >完了済み</a>
     </div>
   );
 }
